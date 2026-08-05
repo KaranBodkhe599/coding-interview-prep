fe68d3a37f89865f803383a21ee09025b53f0a50
Karan Pandurang Bodkhe <karanbodkhe599@gmail.com>
2026-08-05 23:55:29 +0530

    Add Java array demo example
    Add Java/ArrayDemo.java demonstrating basic int array usage: literal initialization (nums), element access and printing, and a fixed-size allocation (x) to show default values and assignment to x[0]. File provides a simple example for array declaration, definition, initialization and indexing.

---
+++ Java/ArrayDemo.java
@@ -0,0 +1,19 @@
+class Array
+{
+     public static void main(String[] args)
+     { 
+	//int array
+	int[] nums = {10,20,30,40,50}; //declaration, defination, intialization
+	System.out.println("First element="+nums[0]);
+	System.out.println("Second element="+nums[1]);
+	System.out.println("Third element="+nums[2]);
+	//OR
+	int x[] = new int[3];
+	System.out.println("First element=" + x[0]);
+	x[0]=99;
+	
+     }
+}
+
+
+
